a372446fa4dd6d56fb31350647eb5a57bd21c151
Renato Opazo <ropazo81@gmail.com>
2021-08-02 02:12:51 +0200

    Finnishing the project

--- learning-how-this-work/Bluetooth/main.py
+++ learning-how-this-work/Bluetooth/main.py
@@ -1,41 +1,56 @@
-from network import Bluetooth
-import pycom
 import time
-import ubinascii
+from network import Bluetooth
 
-pycom.heartbeat(False)
-bluetooth = Bluetooth()
-bluetooth.set_advertisement(name="LoPy4", service_uuid=b'1234567890123456', manufacturer_data="lopy_v1", service_data="Hi world")
 
-# some from: https://forum.pycom.io/topic/6994/find-my-phone-bluetooth-with-my-lopy4/2
+BLEConnected = False
+
 def conn_cb (bt_o):
-    events = bt_o.events()   # this method returns the flags and clears the internal registry
-
-    if events & Bluetooth.CLIENT_CONNECTED:
+    events = bt_o.events()
+    if  events & Bluetooth.CLIENT_CONNECTED:
+        BLEConnected = True
         print("Client connected")
-        #print(bt_o.get_adv())
-        pycom.rgbled(0x001100)
-        adv = bt_o.get_adv()
-        print("All adv data: ",adv)
-        if adv:
-            print("mac: ", ubinascii.hexlify(adv.mac))
-            print("data: ", ubinascii.hexlify(adv.data))
-
-            # try to get the complete name
-            print("Name: ", bluetooth.resolve_adv_data(adv.data, Bluetooth.ADV_NAME_CMPL))
-
-            mfg_data = bluetooth.resolve_adv_data(adv.data, Bluetooth.ADV_MANUFACTURER_DATA)
-            if mfg_data:
-                # try to get the manufacturer data (Apple's iBeacon data is sent here)
-                print(ubinascii.hexlify(mfg_data))
-        time.sleep(3)
-        # bluetooth.disconnect_client()
     elif events & Bluetooth.CLIENT_DISCONNECTED:
+        BLEConnected = False
         print("Client disconnected")
-        pycom.rgbled(0x110000)
-        time.sleep(10)
-        pycom.heartbeat(True)
-        pycom.heartbeat(False)
 
-bluetooth.callback(trigger=Bluetooth.NEW_ADV_EVENT | Bluetooth.CLIENT_CONNECTED | Bluetooth.CLIENT_DISCONNECTED, handler=conn_cb)
-bluetooth.advertise(True)
+def char1_cb_handler(chr, data):
+
+    # The data is a tuple containing the triggering event and the value if the event is a WRITE event.
+    # We recommend fetching the event and value from the input parameter, and not via characteristic.event() and characteristic.value()
+    events, value = data
+    if  events & Bluetooth.CHAR_WRITE_EVENT:
+        print("Write request with value = {}".format(value))
+    else:
+        print('Read request on char 1')
+
+def char2_cb_handler(chr, data):
+    # The value is not used in this callback as the WRITE events are not processed.
+    events, value = data
+    if  events & Bluetooth.CHAR_READ_EVENT:
+        print('Read request on char 2')
+
+bluetooth = Bluetooth()
+bluetooth.set_advertisement(name='LoPy4', service_uuid=b'1234567890123456')
+bluetooth.callback(trigger=Bluetooth.CLIENT_CONNECTED | Bluetooth.CLIENT_DISCONNECTED, handler=conn_cb)
+bluetooth.advertise(True) #this enables the device to advertise that it is here.
+
+srv1 = bluetooth.service(uuid=b'1234567890123456', isprimary=True, nbr_chars=1, start=True) #sets up services
+chr1 = srv1.characteristic(
+    uuid=b'ab34567890123456', 
+    properties=Bluetooth.PROP_INDICATE |
+    Bluetooth.PROP_BROADCAST | 
+    Bluetooth.PROP_NOTIFY, 
+    value=25
+    )  #defining some characteristics
+char1_cb = chr1.callback(trigger=Bluetooth.CHAR_WRITE_EVENT | Bluetooth.CHAR_READ_EVENT, handler=char1_cb_handler)
+
+srv2 = bluetooth.service(uuid=1234, isprimary=True) #sets up another service
+chr2 = srv2.characteristic(uuid=4567, value=0x1234)
+char2_cb = chr2.callback(trigger=Bluetooth.CHAR_READ_EVENT, handler=char2_cb_handler)
+
+
+
+while True:
+    time.sleep(10)
+    if BLEConnected:
+        chr2.value("Hej")
--- project/main.py
+++ project/main.py
@@ -1,7 +1,64 @@
-import machine
-# from machine import Pin
+from machine import Pin
 import time
+import pycom
+# configure the I2C bus
 
-hello_world="Hi All"
+pycom.heartbeat(False)
+
+magnetPin = Pin('P15', mode=Pin.IN)
+ledGreen = Pin('P22', Pin.OUT, pull = Pin.PULL_DOWN)
+ledOrange = Pin('P21', Pin.OUT, pull = Pin.PULL_DOWN)
+ledRed = Pin('P20', Pin.OUT, pull = Pin.PULL_DOWN)
+
+def isMagnetActive(magnetValue):
+    return magnetValue<1
+def ledSwitcher(ledValue):
+    if ledValue == 0:
+        ledGreen.value(0)
+        ledOrange.value(0)
+        ledRed.value(0)
+    elif ledValue == 1:
+        ledGreen.value(1)
+        ledOrange.value(0)
+        ledRed.value(0)
+    elif ledValue == 2:
+        ledGreen.value(0)
+        ledOrange.value(1)
+        ledRed.value(0)
+    elif ledValue == 3:
+        ledGreen.value(0)
+        ledOrange.value(0)
+        ledRed.value(1)
+    else:
+        ledGreen.value(1)
+        ledOrange.value(1)
+        ledRed.value(1)
+
+hello_world="Booting up"
 
 print(hello_world)
+ledSwitcher(4)
+time.sleep(4)
+
+while True:
+  #  Magnet
+  magnetValue = magnetPin()
+
+  if (isMagnetActive(magnetPin())):
+      print("Everything is fine now")
+      ledSwitcher(1)
+      pybytes.send_signal(1, "Close")
+      while isMagnetActive(magnetPin()):
+          time.sleep(5)
+  else :
+      print("The door is open")
+      ledSwitcher(2)
+      pybytes.send_signal(1, "Open")
+      counter = 0
+      while not (isMagnetActive(magnetPin())):
+          time.sleep(1)
+          counter = counter + 1
+          if (counter == 600):
+              print("How long should it be open?")
+              ledSwitcher(3)
+              pybytes.send_signal(1, "Warning Open")
